f36e69d20ffd1609d9c7407cc80ed1d650d3fc8e
huihut <huihut@outlook.com>
2018-03-09 22:41:55 +0800

    修改顺序查找

--- Algorithm/SequentialSearch.h
+++ Algorithm/SequentialSearch.h
@@ -1,9 +1,7 @@
 // 顺序查找
 int SequentialSearch(vector<int>& v, int k) {
-	int i = 0;
-	for (; i < v.size(); ++i)
-		if (v[i] == k) 
+	for (int i = 0; i < v.size(); ++i)
+		if (v[i] == k)
 			return i;
-	if (i == v.size()) 
-		return -1;
+	return -1;
 }
