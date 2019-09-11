e5b43e76320c0f21b8b6e6527d463a4c517aa9ab
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-09-11 22:01:47 +0530

    Change Iterative Function - Merge Print and Iterative function

--- Problems/KnapsackProblem/pack.cpp
+++ Problems/KnapsackProblem/pack.cpp
@@ -2,60 +2,33 @@
 #include <algorithm>
 using namespace std;
 
-int *w = NULL;		// 存储每件物品重量的数组指针
-int *v = NULL;		// 存储每件物品价值的数组指针
-int **T = NULL;		// 存储背包问题表格的数组指针
-int n;				// 物品个数n
-int W;				// 背包总承重W
-
 // 返回两个值的最大值
-int max(int a, int b)
-{
+int max(int a, int b) {
 	return (a > b) ? a : b;
 }
 
 // 迭代法，能显示背包问题的表格
-void packIterative()
-{
+int packIterative(int n, int W, int *w, int *v) {
 	// 循环遍历n行
-	for (int i = 1; i <= n; ++i)
-	{
+	int T[i+1][j+1];
+	for (int i = 0; i <= n; ++i) {
 		// 循环遍历W列
-		for (int j = 1; j <= W; ++j)
-		{
+		for (int j = 0; j <= W; ++j) {
+			if (i == 0 || j == 0) {
+				T[i][j] = 0;
+			}
 			//第i个物品能装下，则比较包括第i个物品和不包括第i个物品，取其最大值
-			if (w[i] <= j)
-				T[i][j] = max(v[i] + T[i - 1][j - w[i]], T[i - 1][j]);
-
+			if ( w[i-1] <= j) {
+				T[i][j] = max(v[i-1] + T[i - 1][j - w[i]], T[i - 1][j]);
+			}
 			// 第i个物品不能装下，则递归装i-1个
-			else
+			else {
 				T[i][j] = T[i - 1][j];
+			}
 		}
 	}
-}
-
-// 递归法，不支持显示背包问题的表格
-int packRecursive(int i, int j, int *w, int *v)
-{
-	// 结束条件（初始条件），i或者j为0时最大总价值为0
-	if (i == 0 || j == 0)
-		return 0;
-
-	// 第i个物品不能装下，则递归装i-1个
-	if (w[i] > j)
-		return packRecursive(i - 1, j, w, v);
-
-	//第i个物品能装下，则比较包括第i个物品和不包括第i个物品，取其最大值
-	else
-		return max(v[i] + packRecursive(i - 1, j - w[i], w, v), packRecursive(i - 1, j, w, v));
-}
-
-// 打印背包问题的表格
-void printT(int n, int W)
-{
-	// 打印n行
-	for (auto i = 0; i <= n; i++)
-	{
+	
+	for (auto i = 0; i <= n; i++) {
 		// 打印行数
 		cout << i << ":\t";
 
@@ -68,10 +41,31 @@ void printT(int n, int W)
 		// 换行
 		cout << endl;
 	}
+	return T[n][W];
 }
 
-int main()
-{
+// 递归法，不支持显示背包问题的表格
+int packRecursive(int n, int W, int *w, int *v) {
+	// 结束条件（初始条件），i或者j为0时最大总价值为0
+	if (n == 0 || W == 0) {
+		return 0;
+	}
+	// 第i个物品不能装下，则递归装i-1个
+	if (w[i] > W) {
+		return packRecursive(n - 1, W, w, v);
+	}
+	//第i个物品能装下，则比较包括第i个物品和不包括第i个物品，取其最大值
+	else {
+		return max(v[i] + packRecursive(n - 1, W - w[n], w , v), packRecursive(n - 1, W, w, v));
+	}
+}
+
+
+int main() {
+	int *w = NULL;		// 存储每件物品重量的数组指针
+	int *v = NULL;		// 存储每件物品价值的数组指针
+	int n;				// 物品个数n
+	int W;				// 背包总承重W
 
 	cout << "---------------- 背包问题 ----------------" << endl;
 	cout << "请输入物品数 n (n>=0) " << endl;
@@ -139,10 +133,7 @@ int main()
 	case 1:
 	{
 		// 迭代法，能显示背包问题的表格
-		packIterative();
-		cout << "能装下物品的最大价值为 " << T[n][W] << endl;
-		cout << "------------------------------------------------" << endl;
-		printT(n, W);
+		cout << "能装下物品的最大价值为 " << packIterative(n, W, w, v) << endl;
 		break;
 	}
 	case 2:
