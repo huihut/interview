d7e235a560e5da8c96044837eba04116a9d5dd33
huihut <huihut@outlook.com>
2020-12-16 15:35:42 +0800

    Update main.cpp
    use English

--- DesignPattern/main.cpp
+++ DesignPattern/main.cpp
@@ -11,30 +11,30 @@
 
 int main() {
     std::cout << "*******************" << std::endl;
-    std::cout << "** ���ģʽ���� **" << std::endl;
+    std::cout << "** Design pattern example **" << std::endl;
     std::cout << "*******************" << std::endl;
 
     std::cout << "*******************" << std::endl;
-    std::cout << "** ����ģʽ **" << std::endl;
+    std::cout << "** Singleton mode **" << std::endl;
     std::cout << "*******************" << std::endl;
     SingletonMain();
 
     std::cout << "*******************" << std::endl;
-    std::cout << "** ���󹤳�ģʽ **" << std::endl;
+    std::cout << "** Abstract factory pattern **" << std::endl;
     std::cout << "*******************" << std::endl;
     FactoryMain();
     
     std::cout << "*******************" << std::endl;
-    std::cout << "** ������ģʽ **" << std::endl;
+    std::cout << "** Adapter mode **" << std::endl;
     std::cout << "*******************" << std::endl;
     AdapterMain();
 
     std::cout << "*******************" << std::endl;
-    std::cout << "** �Ž�ģʽ **" << std::endl;
+    std::cout << "** Bridge mode **" << std::endl;
     std::cout << "*******************" << std::endl;
     
     std::cout << "*******************" << std::endl;
-    std::cout << "** �۲���ģʽ **" << std::endl;
+    std::cout << "** Observer mode **" << std::endl;
     std::cout << "*******************" << std::endl;
     ObserverMain();
 
